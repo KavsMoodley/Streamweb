0469ac4cac67902ba343c20792ae34da2c61b237
KavsMoodley <KavsMoodley@users.noreply.github.com>
2026-08-24 14:30:27 +0200

    analysist

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import Link from "next/link";
 import { Analytics } from "@vercel/analytics/next";
 import SearchBar from "./components/searchBar";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "streamWeb",
